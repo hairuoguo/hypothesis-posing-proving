ab9c1e350d8df3f3e8b86546cc4da900a04f30fc
Simon <simonalford42@gmail.com>
2019-07-03 16:28:41 -0400

    comparing different network sizes

--- dqn/dqn_her.py
+++ dqn/dqn_her.py
@@ -19,13 +19,16 @@ parser.add_argument('-n', '--str_len', default=10, metavar='N', type=int,
         help='string length of Reverse Environment')
 parser.add_argument('-r', '--reverse_len', default=3, metavar='R', type=int, 
         help='length of reversal operation')
-
+parser.add_argument('-a', '--num_layers', default=2, metavar='R', type=int, 
+        help='number of fc layers in network')
+parser.add_argument('-b', '--layer_size', default=128, metavar='R', type=int, 
+        help='layer size in network')
 
 args = parser.parse_args()
 config = Config()
 
-str_len = 10
-reverse_len = 4
+str_len = args.str_len
+reverse_len = args.reverse_len
 reverse_offset = 1
 num_obscured = 0
 
@@ -41,10 +44,10 @@ model_name = str.format('her_{0}_{1}_{2}_{3}', str_len,
  config.file_to_save_session_info) = file_numberer.get_unused_filepaths(data_dir,
          model_name)
 
-config.info = 'viewing ai playing'
+config.info = 'comparing different layer sizes'
 config.seed = 1
-config.num_episodes_to_run = 30
-config.starting_episode_number=50000
+config.num_episodes_to_run = 50
+#config.starting_episode_number=50000
 config.show_solution_score = False
 config.visualise_individual_results = False
 config.standard_deviation_results = 1.0
@@ -59,7 +62,7 @@ config.visualise_overall_agent_results = not config.cluster
 
 config.load_model = True
 config.file_to_load_model = data_dir + '/models/' + model_name + '_(1).pt'
-config.save_at_all = False
+config.save_results = True
 
 
 config.hyperparameters = {
@@ -71,7 +74,7 @@ config.hyperparameters = {
         'discount_rate': 0.999,
         'incremental_td_error': 1e-8,
         'update_every_n_steps': 1,
-        'linear_hidden_units': [128, 128],
+        'linear_hidden_units': [args.layer_size]*args.num_layers,
         'final_layer_activation': None,
 #        'y_range': (0, 1),
         'y_range': (-1, str_len),
