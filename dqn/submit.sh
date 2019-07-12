c672b7fe5bbe25195e05e5b034bf0619375aa84d
Simon <simonalford42@gmail.com>
2019-07-12 09:32:41 -0400

    cnn results 2 july 11 midway

--- dqn/submit.sh
+++ dqn/submit.sh
@@ -1,3 +1,19 @@
-#!/usr/bin/env bash
-# nohup python dqn_her.py > cnn_3_1.out &
-nohup bash test.sh > test.out &
+#!usr/bin/env bash
+nohup python dqn_her.py -n 10 -r 3 -d 3 -e 100000 -s 10000 > /om/user/salford/out/cnn_10_3_3.out &
+nohup python dqn_her.py -n 10 -r 4 -d 3 -e 100000 -s 10000 > /om/user/salford/out/cnn_10_4_3.out &
+nohup python dqn_her.py -n 10 -r 5 -d 3 -e 100000 -s 10000 > /om/user/salford/out/cnn_10_5_3.out &
+nohup python dqn_her.py -n 10 -r 6 -d 3 -e 100000 -s 10000 > /om/user/salford/out/cnn_10_6_3.out &
+nohup python dqn_her.py -n 15 -r 3 -d 3 -e 100000 -s 10000 > /om/user/salford/out/cnn_15_3_3.out &
+nohup python dqn_her.py -n 15 -r 4 -d 3 -e 100000 -s 10000 > /om/user/salford/out/cnn_15_4_3.out &
+nohup python dqn_her.py -n 15 -r 5 -d 3 -e 100000 -s 10000 > /om/user/salford/out/cnn_15_5_3.out &
+nohup python dqn_her.py -n 15 -r 6 -d 3 -e 100000 -s 10000 > /om/user/salford/out/cnn_15_6_3.out &
+
+nohup python dqn_her.py -n 10 -r 3 -d 2 -e 100000 -s 10000 > /om/user/salford/out/cnn_10_3_2.out &
+nohup python dqn_her.py -n 10 -r 6 -d 2 -e 100000 -s 10000 > /om/user/salford/out/cnn_10_6_2.out &
+
+nohup python dqn_her.py -n 10 -r 3 -d 4 -e 100000 -s 10000 > /om/user/salford/out/cnn_10_3_4.out &
+nohup python dqn_her.py -n 10 -r 6 -d 4 -e 100000 -s 10000 > /om/user/salford/out/cnn_10_6_4.out &
+
+nohup python dqn_her.py -n 10 -r 3 -d 5 -e 100000 -s 10000 > /om/user/salford/out/cnn_10_3_5.out &
+nohup python dqn_her.py -n 10 -r 6 -d 5 -e 100000 -s 10000 > /om/user/salford/out/cnn_10_6_5.out &
+
